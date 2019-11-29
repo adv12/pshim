24141a6875a6ac29a99aa227bc61f488202ea061
Andrew Vardeman <andrewdev@the-vardemans.net>
2019-11-29 14:23:07 -0600

    Create EqualizeHistogramCommand.cs

---
+++ src/PShim/EqualizeHistogramCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Management.Automation;
+using SixLabors.ImageSharp.Processing;
+
+namespace PShim
+{
+    [Cmdlet("Equalize", "Histogram", SupportsShouldProcess = true)]
+    public class EqualizeHistogramCommand : FileImageCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            if (!ShouldProcess(FileImage.FileInfo.ToString(), "Equalize Histogram"))
+            {
+                return;
+            }
+            FileImage.Image.Mutate(im => im.HistogramEqualization());
+            WriteObject(FileImage);
+        }
+    }
+}
