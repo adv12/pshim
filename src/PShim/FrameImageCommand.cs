78a6684e5a98b829c8cffad1136fbce91f6335fa
Andrew Vardeman <andrewdev@the-vardemans.net>
2021-01-01 10:21:57 -0600

    I guess I never checked this in

--- src/PShim/FrameImageCommand.cs
+++ src/PShim/FrameImageCommand.cs
@@ -87,8 +87,8 @@ namespace PShim
                     image.Mutate(im => im.Fill(Rgba32.White)
                         .Pad(Math.Max(image.Width, width),
                                 Math.Max(image.Height, height), Rgba32.White)
-                        .Opacity(0)
-                        .Crop(width, height));
+                        .Crop(width, height)
+                        .Opacity(0));
                     PathBuilder pathBuilder = new PathBuilder();
                     List<PointF> outerPoints = new List<PointF>();
                     outerPoints.Add(new PointF(0, 0));
